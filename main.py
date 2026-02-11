2c60d59a70bde6c39d298b2e1169698854f0c41a
aramay <kasper_341@hotmail.com>
2026-02-11 05:54:23 -0800

    added gemini client

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
+import os 
+from google import genai
+from dotenv import load_dotenv
+
 def main():
     print("Hello from gemini-agent!")
 
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    
+    if (not api_key):
+        raise RuntimeError("API_KEY not for gemini")
 
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print("Response:\n")
+    print(response.text)
+    
 if __name__ == "__main__":
     main()
