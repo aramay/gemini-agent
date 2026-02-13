c9dd3dccd23592007968414491e668c677455111
aramay <kasper_341@hotmail.com>
2026-02-13 14:09:38 -0800

    it can now take user input from cmd line

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os 
 from google import genai
 from dotenv import load_dotenv
+import argparse
 
 def main():
     print("Hello from gemini-agent!")
@@ -13,10 +14,26 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    print("args ", args)
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=args.user_prompt
     )
+
+    if not response.usage_metadata:
+        raise RuntimeError("API request failed, not sage meta data found")
+    
+    print("Prompt tokens: ", response.usage_metadata.total_token_count)
+
+    if response.usage_metadata.candidates_token_count:
+        print("Response tokens: ", response.usage_metadata.candidates_token_count)
+        
+        # response.usage_metadata.candidates_token_count
+    # print(response)
     print("Response:\n")
     print(response.text)
     
